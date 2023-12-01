78ab2bd7835f0f086ed678ceeffde866e6794060
Sebastian <anonblade10@gmail.com>
2023-12-01 10:17:56 -0500

    Agregando datos de la BD al modelo

--- modelo.py
+++ modelo.py
@@ -2,17 +2,35 @@
 
 from django.db import models
 
-class Cliente(models.Model):
-    nombre = models.CharField(max_length=100)
-    # Otros campos del cliente
-
-class Producto(models.Model):
-    nombre = models.CharField(max_length=100)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
-    # Otros campos del producto
+class Empresa(models.Model):
+    id_empresa = models.AutoField(primary_key=True)
+    razon_social = models.CharField(max_length=255)
+    ruc = models.CharField(max_length=20)
+    direccion_matriz = models.CharField(max_length=255)
+    direccion_sucursal = models.CharField(max_length=255)
 
 class Factura(models.Model):
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    productos = models.ManyToManyField(Producto)
+    id_factura = models.AutoField(primary_key=True)
     fecha_emision = models.DateField()
-    # Otros campos de la factura
+    guia_remision = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=255)
+    total_pagar = models.DecimalField(max_digits=10, decimal_places=2)
+    id_empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+
+class FormaPago(models.Model):
+    id_forma_pago = models.AutoField(primary_key=True)
+    forma_pago = models.CharField(max_length=255)
+
+class Pago(models.Model):
+    id_pago = models.AutoField(primary_key=True)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_pago = models.DateField()
+    id_forma_pago = models.ForeignKey(FormaPago, on_delete=models.CASCADE)
+    id_factura = models.ForeignKey(Factura, on_delete=models.CASCADE)
+
+class FacturaAdquirente(models.Model):
+    id_factura_adquirente = models.AutoField(primary_key=True)
+    nombre_adquirente = models.CharField(max_length=255)
+    ruc_adquirente = models.CharField(max_length=20)
+    id_factura = models.ForeignKey(Factura, on_delete=models.CASCADE)
+
