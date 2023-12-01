6ed91049a9031fa3e594253b5c2b33643c675f8e
HeyMrDave <nuevobinalv@gmail.com>
2023-12-01 10:32:22 -0500

    crud

--- crud.py
+++ crud.py
@@ -1,23 +1,56 @@
-# CRUD para las Facturas 
-def create_factura(num_factura, fecha_emision, ruc, nombre_cliente, monto_total):
-    factura = Factura(num_factura=num_factura, fecha_emision=fecha_emision, ruc=ruc, nombre_cliente=nombre_cliente, monto_total=monto_total)
-    session.add(factura)
-    session.commit()
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extras import RealDictCursor
+from settings import DATABASE_PATH
 
-def update_factura(id, num_factura, fecha_emision, ruc, nombre_cliente, monto_total):
-    factura = session.query(Factura).filter_by(id=id).first()
-    factura.num_factura = num_factura
-    factura.fecha_emision = fecha_emision
-    factura.ruc = ruc
-    factura.nombre_cliente = nombre_cliente
-    factura.monto_total = monto_total
-    session.commit()
+# Conexión a la base de datos
+def conectar():
+    return psycopg2.connect(DATABASE_PATH)
 
-def delete_factura(id):
-    factura = session.query(Factura).filter_by(id=id).first()
-    session.delete(factura)
-    session.commit()
+# Operaciones CRUD para la tabla Empresa
+def crear_empresa(razon_social, ruc, direccion_matriz, direccion_sucursal):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Empresa (razon_social, ruc, direccion_matriz, direccion_sucursal) VALUES (%s, %s, %s, %s) RETURNING id_empresa",
+                   (razon_social, ruc, direccion_matriz, direccion_sucursal))
+    id_empresa = cursor.fetchone()[0]
+    conn.commit()
+    conn.close()
+    return id_empresa
 
-def read_factura(id):
-    factura = session.query(Factura).filter_by(id=id).first()
-    return factura
+def obtener_empresas():
+    conn = conectar()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("SELECT * FROM Empresa")
+    empresas = cursor.fetchall()
+    conn.close()
+    return empresas
+
+# Puedes agregar funciones similares para actualizar y eliminar empresas si es necesario.
+
+# Operaciones CRUD para la tabla Factura
+def crear_factura(fecha_emision, guia_remision, descripcion, total_pagar, id_empresa):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Factura (fecha_emision, guia_remision, descripcion, total_pagar, id_empresa) VALUES (%s, %s, %s, %s, %s) RETURNING id_factura",
+                   (fecha_emision, guia_remision, descripcion, total_pagar, id_empresa))
+    id_factura = cursor.fetchone()[0]
+    conn.commit()
+    conn.close()
+    return id_factura
+
+def obtener_facturas():
+    conn = conectar()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("SELECT * FROM Factura")
+    facturas = cursor.fetchall()
+    conn.close()
+    return facturas
+
+# Puedes agregar funciones similares para actualizar y eliminar facturas si es necesario.
+
+# Operaciones CRUD para las otras tablas (Forma_Pago, Pago, Factura_Adquirente) de manera similar.
+
+if _name_ == "_main_":
+    # Aquí puedes probar las funciones según tus necesidades.
+    pass
